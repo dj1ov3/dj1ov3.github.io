40ac9ff89d48aaff82b8dc6c82d74a82b626889f
David T. Juarez Jr <djuarez1003@gmail.com>
2019-04-15 12:28:16 -0500

    added scroll on navbar

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,12 @@
 console.log("wus good");
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navDiv").style.top = "0";
+  } else {
+    document.getElementById("navDiv").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
