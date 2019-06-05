3f518d1140699392abe330fb95492f005604acda
David T. Juarez Jr <djuarez1003@gmail.com>
2019-06-05 11:18:42 -0500

    collapse feature

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,21 @@
-console.log("wus good");
-
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navDiv").style.top = "0";
-  } else {
-    document.getElementById("navDiv").style.top = "-50px";
+$(document).ready(function(){
+  console.log("wus good");
+  //Scroll function for navbar
+  var prevScrollpos = window.pageYOffset;
+  window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("navDiv").style.top = "0";
+    } else {
+      document.getElementById("navDiv").style.top = "-50px";
+    }
+    prevScrollpos = currentScrollPos;
   }
-  prevScrollpos = currentScrollPos;
-}
+  //Hides the other options when clicking on projects
+  $('.projects').on('show.bs.collapse', function() {
+    $(this).siblings().children().collapse('hide');
+ });
+
+})
+
+
